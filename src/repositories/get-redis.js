beba3ea1613cd527f452275259fabcfdb423f734
Layssa <layssa.lima166@gmail.com>
2022-02-20 23:40:12 -0300

    create functions login

--- src/repositories/get-redis.js
+++ src/repositories/get-redis.js
@@ -1,6 +1,8 @@
 const { redisClient } = require("../database/_redis");
 
 async function getDataRedis(_search) {
+  console.log('GET REDIS');
+  console.log(_search);
   return await redisClient.get(_search);
 }
 
