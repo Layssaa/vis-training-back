3af5fa81037174616c787181005f0c3862342293
Layssa <layssa.lima166@gmail.com>
2022-02-21 11:11:30 -0300

    remove logs

--- src/repositories/set-redis.js
+++ src/repositories/set-redis.js
@@ -1,10 +1,6 @@
 const { redisClient } = require("../database/_redis");
 
 async function setDataRedis(_key, _send) {
-  console.log("SET REDIS");
-  console.log(_key);
-  console.log(_send);
-  
   return await redisClient.set(_key, JSON.stringify(_send));
 }
 
