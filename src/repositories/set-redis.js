977e0e4f0cfd3430dc4eee88da111ad257e73999
Layssa <layssa.lima166@gmail.com>
2022-02-25 22:45:29 -0300

    update redis

--- src/repositories/set-redis.js
+++
@@ -1,7 +0,0 @@
-const { redisClient } = require("../database/_redis");
-
-async function setDataRedis(_key, _send) {
-  return await redisClient.set(_key, JSON.stringify(_send));
-}
-
-module.exports = { setDataRedis };
