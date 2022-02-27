f8c4cff4f63dba2e3ec491e311d0f0e3ba9150b8
Layssa <layssa.lima166@gmail.com>
2022-02-27 17:18:17 -0300

    fix: server midd

--- src/server.js
+++ src/server.js
@@ -6,12 +6,13 @@ const { _getData, _login, _registerUser } = require("./routers/index");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res) => {
-  console.log("==> ",req.url);
+app.use((req, res, next) => {
+  console.log("==> ", req.url);
+  next()
 });
 
-app.use(_login);
+// app.use(_login);
 app.use(_getData);
-app.use(_registerUser);
+// app.use(_registerUser);
 
 module.exports = app;
