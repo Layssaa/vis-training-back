e7aa64ba36b01e1ed743cc0dd4913e1bba78e6ea
Layssa <layssa.lima166@gmail.com>
2022-02-27 17:17:56 -0300

    fix: login changes

--- src/usecase/login.js
+++ src/usecase/login.js
@@ -12,16 +12,16 @@ async function loginUsecase(_user) {
   try {
     const ifUserLogged = await getDataRedis(`use-${email}`);
 
-    if (!ifUserLogged) {
+    if (!ifUserLogged) { 
       dataUser = await findUserMongoDB({ email: email });
 
-      if (dataUser.length == 0) throw new Error("User not found");
+      if (dataUser.length == 0 || !dataUser) throw new Error("User not found");
 
       if (!(await AuthenticateUser(password, email, dataUser[0].password))) {
         throw new Error("Incorrect data");
       }
 
-      await setDataRedis(`use-${email}`, _user);
+      await setDataRedis(`use-${email}`, dataUser[0].id);
     }
 
     return { data: ifUserLogged || dataUser[0] };
