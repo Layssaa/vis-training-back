1c9daacc0eaac55e71f9e0a1df84f0be30073985
SousaGLucas <lucasgsousa93@gmail.com>
2022-03-14 22:05:44 -0300

    refactor: update mongodb config bash files

--- src/bash/mongo-create-config-files.sh
+++ src/bash/mongo-create-config-files.sh
@@ -15,17 +15,15 @@ systemLog:
 \n   bindIpAll: true
 \n   port: 27017
 \n   maxIncomingConnections: 5
-\nsetParameter:
-\n   enableLocalhostAuthBypass: false
 \nreplication:
 \n   replSetName: vis
 \nsecurity:
 \n   authorization: enabled
-\n   keyFile: /srv/mongodb/keyfile
+\n   keyFile: /srv/mongodb/p/keyfile
 "
 
 SECONDARY1="
-\nsystemLog:
+systemLog:
 \n   destination: file
 \n   path: "/var/log/mongodb/s1/mongod.log"
 \n   logAppend: true
@@ -39,13 +37,11 @@ SECONDARY1="
 \n   bindIpAll: true
 \n   port: 27020
 \n   maxIncomingConnections: 5
-\nsetParameter:
-\n   enableLocalhostAuthBypass: false
 \nreplication:
 \n   replSetName: vis
 \nsecurity:
 \n   authorization: enabled
-\n   keyFile: /srv/mongodb/keyfile
+\n   keyFile: /srv/mongodb/s2/keyfile
 "
 
 SECONDARY2="
@@ -63,13 +59,11 @@ systemLog:
 \n   bindIpAll: true
 \n   port: 27021
 \n   maxIncomingConnections: 5
-\nsetParameter:
-\n   enableLocalhostAuthBypass: false
 \nreplication:
 \n   replSetName: vis
 \nsecurity:
 \n   authorization: enabled
-\n   keyFile: /srv/mongodb/keyfile
+\n   keyFile: /srv/mongodb/s1/keyfile
 "
 
 SECRET="7/eeIF5w9b9wkbiPJubTGifGet7HPJO6aPmrRP0wKI/sHkpSWv5+dojVToHuT1MNhEwC+36taiklGG02Sfq814KP7PAqewBw10c51NIwBdQoxMH2ZmtH7aOw6RIS2WIutkiKpbyEnfEZUSS7O8WoxIfL6h/yQlVMzt48bsEi2gCKOLgLK8XbQP3vT6E+HAgfcjgwUkXAQimRCKDAsZtDaG/6DnGRALBCHlPA90NQ/T/lLsgH+/pMSBIbWph8bv+1tb+8SMsZvZ+qFd3OQx9V3V6p4ajBJF0J0ZnnUNJdNy7zf/QCEel6sES9eJgp5jTx+juFVfuNJZPDtwnolqOd+QILb+jMzcn2GQ0MvHwZGOwEBEuPD9EXJJMRLDqNnv33CWAW+8vpMK/GRZLWuZa/p3nT+CvfEMUtPdzK/wAVgmM06B9VEZ04BhsY8Sg2aDLi09mn/DKp05whfiDFfs92NY5H02UrAwGxxvPoT92g3HH8FB2tGjmrG07CRI6ntd7T90hwuYUUxtHG06qTARvKPbN17ExvN4rJWonrkIHGs/S9nbyccv2td0lzqpYfp+AeZNKhGosiHc6sYdV3zfNqRp0iXskgiFJC+TC+wN9a56lHSRxmaVQXvrXVdidzJm1ytmITPqHVsZ0eylG5meP78NO49O/8y+jsI8Hy5vAJvN7L6QYtzcUh4KRxvKKV1Fl62EloeEfeOhc0gJlcdTm/yaMjxE4O/7dWmH3u39eXQCDqaRYMMIkPEUyk7kj2EpnqqgD4Kr8emRVmXHGqRO0oPMdcSMtLY7fxV/IYSkYEsgIrxDdppWy9EZcfiryodfZufVjTEDuxzJ9IuweXN3b0avGjkty25d7RLnOLda0k9zDA2LTgnXq+YoVm1i5pcFYVI6MpqcfgVWfr4Smw0gLQASGyLAKGkvfasn+42Go38zLPIe1Lvb054JGvL3ZbYI+fi5O6G3KcuG+AVJ3h8aQ356NaEQTwP3/M6DzC2SYm67RcYtHP"
--- src/bash/mongo-create-directories.sh
+++ src/bash/mongo-create-directories.sh
@@ -2,10 +2,13 @@
 
 createDirectories() {
     mkdir /etc/mongo
-    mkdir /ect/mongo/p
-    mkdir /ect/mongo/s1
-    mkdir /ect/mongo/s2
+    mkdir /etc/mongo/p
+    mkdir /etc/mongo/s1
+    mkdir /etc/mongo/s2
     mkdir /srv/mongodb/
+    mkdir /srv/mongodb/p
+    mkdir /srv/mongodb/s1
+    mkdir /srv/mongodb/s2
     mkdir /var/log/mongodb
     mkdir /var/log/mongodb/p
     mkdir /var/log/mongodb/s1
