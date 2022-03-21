c2cffd5e40fd12dd2874002ac906a3e6dc049fb2
Layssa <layssa.lima166@gmail.com>
2022-03-21 01:10:44 -0300

    create: files to router get training by date

--- src/controllers/get-trainings.js
+++ src/controllers/get-trainings.js
@@ -1,5 +1,18 @@
-async function getTrainingsController(req, res){
-    console.log("GET TRAINING");
+import { getTrainingsUsecase } from "../usecase/get-trainings.js";
+
+async function getTrainingsController(req, res) {
+  const { from, to, modality, token } = req.query;
+
+  try {
+    const { data, error } = await getTrainingsUsecase({ from, to, modality, token });
+
+    if (error) throw new Error(error);
+    res.status(200).send(data);
+
+  } catch (error) {
+      console.log(error);
+    res.status(200).send("erooooooou");
+  }
 }
 
-export { getTrainingsController }
+export { getTrainingsController };
