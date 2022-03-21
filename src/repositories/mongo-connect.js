b1a4c2edca92c963ac929a42b0e15e683a7ba965
Layssa <layssa.lima166@gmail.com>
2022-03-21 01:19:08 -0300

    create: sort result

--- src/repositories/mongo-connect.js
+++ src/repositories/mongo-connect.js
@@ -73,27 +73,27 @@ async function updateDataMongoDB(_type, _findId, _obj) {
 async function findByDate({ from, to, modality, id }) {
   const userFounded = await User.findOne({ _id: id });
 
-  const result = userFounded.modalities[modality].records.filter((elem) => {
-    if(!elem.date){
-      return
+  let result = userFounded.modalities[modality].records.filter((elem) => {
+    if (!elem.date) {
+      return;
     }
 
-    const biggerThen =
-      changeTimeToMilliseconds(elem.date) >= changeTimeToMilliseconds(from);
-    const lessThan =
-      changeTimeToMilliseconds(elem.date) <= changeTimeToMilliseconds(to);
-
+    const biggerThen = toMilliseconds(elem.date) >= toMilliseconds(from);
+    const lessThan = toMilliseconds(elem.date) <= toMilliseconds(to);
 
     if (biggerThen && lessThan) {
       return elem;
     }
   });
 
-  return result
+  result = result.sort(
+    (date1, date2) => toMilliseconds(date1) >= toMilliseconds(date2) ? 1 : -1
+  );
+
+  return result;
 }
 
-
-function changeTimeToMilliseconds(_time) {
+function toMilliseconds(_time) {
   return new Date(_time);
 }
 
