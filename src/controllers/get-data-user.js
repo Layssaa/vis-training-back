559d0e0e3345a7684c75ba3e8b068209d3479eb4
Lay <layssa.lima166@gmail.com>
2022-03-23 13:46:24 -0300

    create: router return data  user (#26)
    * create:function  return data user with timestamp
    
    * create:pushe new updated

--- src/controllers/get-data-user.js
+++ src/controllers/get-data-user.js
@@ -1,6 +1,25 @@
+import { getDataUsecase } from "../usecase/get-data-user.js";
+import { responseStatus, responseMessages } from "../constants/index.js";
+
 async function getDataController(req, res) {
-  console.log("GET DATA");
-  res.send("data");
+  const { token } = req.params;
+  try {
+    const { status, data, error } = await getDataUsecase({ token });
+
+    if (status !== responseStatus.ok) {
+      // criar log de errors
+      res.status(status).send({ error });
+    } else {
+      res.status(status).send({ data });
+    }
+  } catch (error) {
+    // criar log de errors
+    console.log(error);
+
+    res
+      .status(responseStatus.internal_server_error)
+      .send({ error: responseMessages.internal_server_error });
+  }
 }
 
 export { getDataController };
--- src/repositories/redis-connect.js
+++ src/repositories/redis-connect.js
@@ -8,6 +8,10 @@ async function setDataRedis(_key, _send) {
   return await redisClient.set(_key, JSON.stringify(_send));
 }
 
+async function setDataWithTimestamp(_key, _send, _time) {
+  return await redisClient.set(_key, JSON.stringify(_send), "EX", _time);
+}
+
 async function setSessionRedis(_key, _send) {
   return await redisClient.set(_key, JSON.stringify(_send), "EX", 3600);
 }
@@ -16,4 +20,10 @@ async function deleteSessionRedis(_key) {
   return await redisClient.del(_key);
 }
 
-export { getDataRedis, setDataRedis, setSessionRedis, deleteSessionRedis };
+export {
+  getDataRedis,
+  setDataRedis,
+  setSessionRedis,
+  deleteSessionRedis,
+  setDataWithTimestamp,
+};
